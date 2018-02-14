cd14931deaaa8149ff8c78605be19c2e8b07a279
Guido Vranken <guidovranken@gmail.com>
2018-02-14 16:17:43 +0100

    OpenSSL module: wrap each NOP test in separate function

--- modules/openssl/module.c
+++ modules/openssl/module.c
@@ -81,6 +81,55 @@ static void destroy_bignum(void* bignum)
     BN_free(bn);
 }
 
+static void test_bn_sqrx8x_internal(const BIGNUM *B, const BIGNUM *C)
+{
+    /* Test for bn_sqrx8x_internal carry bug on x86_64 (CVE-2017-3736) */
+    if ( BN_cmp(C, B) >= 0 &&
+            BN_cmp(B, zero) > 0 ) /* this automatically implies that C is also positive */ {
+        BN_MONT_CTX* mont = BN_MONT_CTX_new();
+        BIGNUM* Bcopy = BN_dup(B);
+        if ( BN_MONT_CTX_set(mont, C, ctx) != 0 ) {
+            BIGNUM* x = BN_new();
+            if ( BN_mod_mul_montgomery(x, B, B, mont, ctx) != 0 ) {
+                BIGNUM* y = BN_new();
+                if ( BN_mod_mul_montgomery(y, B, Bcopy, mont, ctx) != 0 ) {
+                    if ( BN_cmp(x, y) != 0 ) {
+                        abort();
+                    }
+                }
+                BN_free(y);
+            }
+            BN_free(x);
+        }
+        BN_MONT_CTX_free(mont);
+        BN_free(Bcopy);
+    }
+}
+
+static void test_rsaz_1024_mul_avx2(const BIGNUM* A, const BIGNUM *B, const BIGNUM *C)
+{
+    /* Test for rsaz_1024_mul_avx2 overflow bug on x86_64 (CVE-2017-3738) */
+    if ( BN_cmp(C, zero) > 0 &&
+            BN_cmp(B, zero) > 0 &&
+            BN_cmp(A, zero) > 0 ) {
+        BN_MONT_CTX* mont = BN_MONT_CTX_new();
+        if ( BN_MONT_CTX_set(mont, C, ctx) != 0 ) {
+            BIGNUM* x = BN_new();
+            if ( BN_mod_exp_mont_consttime(x, A, B, C, ctx, mont) != 0 ) {
+                BIGNUM* y = BN_new();
+                if ( BN_mod_exp_mont(y, A, B, C, ctx, mont) != 0 ) {
+                    if ( BN_cmp(x, y) != 0 ) {
+                        abort();
+                    }
+                }
+                BN_free(y);
+            }
+            BN_free(x);
+        }
+        BN_MONT_CTX_free(mont);
+    }
+}
+
 static void test_BN_mod_sqrt(const BIGNUM *B, const BIGNUM *C)
 {
     BIGNUM* tmp1 = BN_new();
@@ -408,53 +457,10 @@ static int operation(
                 ret = 0;
                 switch ( opt ) {
                     case    0:
-                        {
-                            /* Test for bn_sqrx8x_internal carry bug on x86_64 (CVE-2017-3736) */
-                            if ( BN_cmp(C, B) >= 0 &&
-                                    BN_cmp(B, zero) > 0 ) /* this automatically implies that C is also positive */ {
-                                BN_MONT_CTX* mont = BN_MONT_CTX_new();
-                                BIGNUM* Bcopy = BN_dup(B);
-                                if ( BN_MONT_CTX_set(mont, C, ctx) != 0 ) {
-                                    BIGNUM* x = BN_new();
-                                    if ( BN_mod_mul_montgomery(x, B, B, mont, ctx) != 0 ) {
-                                        BIGNUM* y = BN_new();
-                                        if ( BN_mod_mul_montgomery(y, B, Bcopy, mont, ctx) != 0 ) {
-                                            if ( BN_cmp(x, y) != 0 ) {
-                                                abort();
-                                            }
-                                        }
-                                        BN_free(y);
-                                    }
-                                    BN_free(x);
-                                }
-                                BN_MONT_CTX_free(mont);
-                                BN_free(Bcopy);
-                            }
-                        }
+                        test_bn_sqrx8x_internal(B, C);
                         break;
                     case    1:
-                        {
-                            /* Test for rsaz_1024_mul_avx2 overflow bug on x86_64 (CVE-2017-3738) */
-                            if ( BN_cmp(C, zero) > 0 &&
-                                    BN_cmp(B, zero) > 0 &&
-                                    BN_cmp(A, zero) > 0 ) {
-                                BN_MONT_CTX* mont = BN_MONT_CTX_new();
-                                if ( BN_MONT_CTX_set(mont, C, ctx) != 0 ) {
-                                    BIGNUM* x = BN_new();
-                                    if ( BN_mod_exp_mont_consttime(x, A, B, C, ctx, mont) != 0 ) {
-                                        BIGNUM* y = BN_new();
-                                        if ( BN_mod_exp_mont(y, A, B, C, ctx, mont) != 0 ) {
-                                            if ( BN_cmp(x, y) != 0 ) {
-                                                abort();
-                                            }
-                                        }
-                                        BN_free(y);
-                                    }
-                                    BN_free(x);
-                                }
-                                BN_MONT_CTX_free(mont);
-                            }
-                        }
+                        test_rsaz_1024_mul_avx2(A, B, C);
                         break;
                     case    2:
                         test_BN_mod_sqrt(B, C);
