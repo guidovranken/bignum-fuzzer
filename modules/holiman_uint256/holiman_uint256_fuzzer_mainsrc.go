5e2cabeb448644e3eea86078aa0adfbc4497c213
Guido Vranken <guidovranken@gmail.com>
2018-11-07 21:13:40 +0100

    Extend holiman_uint256 module

--- modules/holiman_uint256/holiman_uint256_fuzzer_mainsrc.go
+++ modules/holiman_uint256/holiman_uint256_fuzzer_mainsrc.go
@@ -41,10 +41,11 @@ func HolimanUint256Initialize(coverTabPtr unsafe.Pointer, coverTabSize uint64, m
 
 //export HolimanUint256BignumFromString
 func HolimanUint256BignumFromString(s string, bn_index int) int {
-    B, err := new(big.Int).SetString(s, 10)
-    if err == true {
+    B, ok := new(big.Int).SetString(s, 10)
+    if ok == false {
         return -1
     }
+    var err bool
     g_nums[bn_index], err = target.FromBig(B)
     if err == true {
         return -1
@@ -153,11 +154,7 @@ func op_LSHIFT(A int, B int, C int, D int, direct bool) int {
 }
 
 func op_RSHIFT(A int, B int, C int, D int, direct bool) int {
-    /* TODO
-    g_nums[A].Set(g_nums[B])
-    if g_nums[A].Cmp(target.NewInt()) < 0 && g_nums[A].Bit(0) == 1 {
-        g_nums[A].Add(g_nums[A], big.NewInt(1))
-    }
+    g_nums[A].Copy(g_nums[B])
 
     if direct {
         g_nums[A].Rsh(g_nums[A], 1)
@@ -167,27 +164,6 @@ func op_RSHIFT(A int, B int, C int, D int, direct bool) int {
         g_nums[A] = tmp
     }
     return 0
-    */
-
-    return -1
-}
-
-func op_GCD(A int, B int, C int, D int, direct bool) int {
-    /* TODO
-    if g_nums[B].Cmp(target.NewInt()) > 0 && g_nums[C].Cmp(target.NewInt()) > 0 {
-        if direct {
-            g_nums[A].GCD(nil, nil, g_nums[B], g_nums[C])
-        } else {
-            tmp := target.NewInt()
-            tmp.GCD(nil, nil, g_nums[B], g_nums[C])
-            g_nums[A] = tmp
-        }
-        return 0
-    } else {
-        return -1
-    }
-    */
-    return -1
 }
 
 func op_MOD_ADD(A int, B int, C int, D int, direct bool) int {
@@ -208,49 +184,44 @@ func op_MOD_ADD(A int, B int, C int, D int, direct bool) int {
 }
 
 func op_EXP(A int, B int, C int, D int, direct bool) int {
-    /* TODO
-    thousand := big.NewInt(1000)
+    thousand := target.NewInt().SetUint64(1000)
     if g_nums[B].Cmp(target.NewInt()) > 0 && g_nums[B].Cmp(thousand) < 0 && g_nums[C].Cmp(target.NewInt()) > 0 && g_nums[C].Cmp(thousand) < 0 {
         if direct {
-            g_nums[A].Exp(g_nums[B], g_nums[C], nil)
+            g_nums[A].Exp(g_nums[B], g_nums[C])
             return 0
         } else {
             tmp := target.NewInt()
-            tmp.Exp(g_nums[B], g_nums[C], nil)
+            tmp.Exp(g_nums[B], g_nums[C])
             g_nums[A] = tmp
             return 0
         }
     } else {
         return -1
     }
-    */
     return -1
 }
 
 
 func op_CMP(A int, B int, C int, D int, direct bool) int {
-    /* TODO
     res := 0
     if direct {
         res = g_nums[B].Cmp(g_nums[C])
     } else {
         tmp := target.NewInt()
-        tmp.Set(g_nums[B])
+        tmp.Copy(g_nums[B])
         res = tmp.Cmp(g_nums[C])
     }
 
     if res > 0 {
-        g_nums[A] = big.NewInt(1)
+        g_nums[A].SetUint64(2)
     } else {
         if res == 0 {
-            g_nums[A] = target.NewInt()
+            g_nums[A].SetUint64(1)
         } else {
-            g_nums[A] = big.NewInt(-1)
+            g_nums[A].SetUint64(0)
         }
     }
     return 0
-    */
-    return -1
 }
 
 func op_SQR(A int, B int, C int, D int, direct bool) int {
@@ -268,59 +239,19 @@ func op_SQR(A int, B int, C int, D int, direct bool) int {
 }
 
 func op_NEG(A int, B int, C int, D int, direct bool) int {
-    /* TODO
-    if direct {
-        g_nums[A].Neg(g_nums[B])
-    } else {
-        tmp := target.NewInt()
-        tmp.Neg(g_nums[B])
-        g_nums[A] = tmp
-    }
+    tmp := target.NewInt().Copy(g_nums[B])
+    g_nums[B].Neg()
+    g_nums[A].Copy(g_nums[B])
+    g_nums[B].Copy(tmp)
     return 0
-    */
-    return -1
 }
 
 func op_ABS(A int, B int, C int, D int, direct bool) int {
-    /* TODO
-    if direct {
-        g_nums[A].Abs(g_nums[B])
-    } else {
-        tmp := target.NewInt()
-        tmp.Abs(g_nums[B])
-        g_nums[A] = tmp
-    }
+    tmp := target.NewInt().Copy(g_nums[B])
+    g_nums[B].Abs()
+    g_nums[A].Copy(g_nums[B])
+    g_nums[B].Copy(tmp)
     return 0
-    */
-    return -1
-}
-
-func op_IS_PRIME(A int, B int, C int, D int, direct bool) int {
-    /* "ProbablyPrime is 100% accurate for inputs less than 2⁶⁴."
-     * https://golang.org/pkg/math/big/#Int.ProbablyPrime
-    */
-    /* TODO?
-    max64 := target.NewInt().Lsh( big.NewInt(1), 64 )
-    max64.Sub(max64, big.NewInt(1))
-    if g_nums[B].Cmp(target.NewInt()) > 0 && g_nums[B].Cmp(max64) < 0 {
-        is_prime := false
-        if direct {
-            is_prime = g_nums[B].ProbablyPrime(1)
-        } else {
-            tmp := target.NewInt().Set(g_nums[B])
-            is_prime = tmp.ProbablyPrime(1)
-        }
-        if is_prime {
-            g_nums[A] = big.NewInt(1)
-        } else {
-            g_nums[A] = target.NewInt()
-        }
-        return 0
-    } else {
-        return -1
-    }
-    */
-    return -1
 }
 
 func op_MOD_SUB(A int, B int, C int, D int, direct bool) int {
@@ -399,14 +330,12 @@ func HolimanUint256BignumOperation(op int, A int, B int, C int, D int, opt int) 
     op == BN_FUZZ_OP_EXP_MOD { return op_EXP_MOD(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_LSHIFT { return op_LSHIFT(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_RSHIFT { return op_RSHIFT(A, B, C, D, direct) } else if
-    op == BN_FUZZ_OP_GCD { return op_GCD(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_MOD_ADD { return op_MOD_ADD(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_EXP { return op_EXP(A, B, C, D, direct); } else if
     op == BN_FUZZ_OP_CMP { return op_CMP(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_SQR { return op_SQR(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_NEG { return op_NEG(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_ABS { return op_ABS(A, B, C, D, direct) } else if
-    op == BN_FUZZ_OP_IS_PRIME { return op_IS_PRIME(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_MOD_SUB { return op_MOD_SUB(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_SWAP { return op_SWAP(A, B, C, D, direct) } else if
     op == BN_FUZZ_OP_MOD_MUL { return op_MOD_MUL(A, B, C, D, direct) } else if
